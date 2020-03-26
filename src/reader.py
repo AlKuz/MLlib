548d9ab91fa6dfff5ef9b78b5e70925c93fddb86
Aleksandr Kuznetsov <aleksandr_kuznetsov3@epam.com>
2020-03-26 15:15:51 +0400

    Transferred batching method in the base class

--- src/reader.py
+++ src/reader.py
@@ -1,32 +1,37 @@
 import pandas as pd
 import os
-from copy import copy
+from copy import deepcopy
 from abc import ABC, abstractmethod
 from typing import Optional, List, Callable, Iterator, Any
 
 
 class Reader(ABC):
 
-    def __init__(self, folder: str, file_filter: Optional[Callable] = None, sorted_key: Optional[Callable] = None):
-        self._files = sorted(self._get_files_paths(folder, file_filter), key=sorted_key)
-        self._iterator = self._create_iterator()
+    def __init__(self, folder: str,
+                 file_filter: Optional[Callable] = None,
+                 sorted_key: Optional[Callable] = None,
+                 batch_size: int = -1):
+        assert batch_size > 0 or batch_size == -1, f"Batch size should be > 0 or == -1, received {batch_size}"
+        self.__files = sorted(self.__get_files_paths(folder, file_filter), key=sorted_key)
+        self.__batch_size = batch_size
+        self.__batch = []
 
     def __iter__(self):
-        return self
-
-    def __next__(self):
-        return next(self._iterator)
+        for step in self._create_iterator(self.__files):
+            batch = self.__compile_batch(step)
+            if batch is not None:
+                yield batch
 
     def __len__(self):
-        return len(self._files)
+        return len(self.__files)
 
     def __getitem__(self, item):
-        other = copy(self)
-        other._files = other._files[item]
+        other = deepcopy(self)
+        other.__files = other.__files[item]
         return other
 
     @staticmethod
-    def _get_files_paths(folder: str, file_filter: Callable) -> List[str]:
+    def __get_files_paths(folder: str, file_filter: Callable) -> List[str]:
         paths_list = []
         for root, folders_list, files_list in os.walk(os.path.abspath(folder)):
             for file in files_list:
@@ -35,43 +40,48 @@ class Reader(ABC):
             paths_list = list(filter(file_filter, paths_list))
         return paths_list
 
+    def __compile_batch(self, data: Any):
+        if self.__batch_size == -1:
+            return data
+        elif len(self.__batch) == self.__batch_size:
+            result, self.__batch = self.__batch, []
+            return result
+        else:
+            self.__batch.append(data)
+
     @abstractmethod
-    def _create_iterator(self) -> Iterator[Any]:
+    def _create_iterator(self, files_paths: List[str]) -> Iterator:
         raise NotImplemented
 
 
-class CSVReader(Reader):
+class TimeSeriesCSVReader(Reader):
 
     def __init__(self, folder: str,
                  file_filter: Optional[Callable] = None,
                  sorted_key: Optional[Callable] = None,
-                 chunk_size: int = 10000,
-                 batch_size: int = 1):
-        super().__init__(folder, file_filter, sorted_key)
+                 batch_size: int = -1,
+                 chunk_size: int = 10000):
+        super().__init__(folder, file_filter, sorted_key, batch_size)
         self._chunk_size = chunk_size
-        self._batch_size = batch_size
-        self._batch = []
 
-    def _create_iterator(self) -> Iterator[List[dict]]:
-        for file_path in self._files:
+    def _create_iterator(self, files_paths: List[str]) -> Iterator[dict]:
+        """
+        Create processing file iterator
+        Returns:
+            Iterator[dict]
+        """
+        for file_path in files_paths:
             for data in pd.read_csv(file_path, error_bad_lines=False, chunksize=self._chunk_size):
                 for d in data.iterrows():
-                    d = d[1].to_dict()
-                    batch = self._compile_batch(d)
-                    if batch is not None:
-                        yield batch
+                    yield d[1].to_dict()
 
-    def _compile_batch(self, data: dict):
-        if len(self._batch) != self._batch_size:
-            self._batch.append(data)
-        else:
-            result, self._batch = self._batch, []
-            return result
+
+
 
 
 if __name__ == "__main__":
-    FOLDER = r"C:\Projects\INNC-MLFR\data\raw"
-    reader = CSVReader(
+    FOLDER = r"C:\Projects\Customers\ConocoFillips\data\raw\crosslines"
+    reader = TimeSeriesCSVReader(
         folder=FOLDER,
         file_filter=lambda x: "log" in x.split("_")
     )
