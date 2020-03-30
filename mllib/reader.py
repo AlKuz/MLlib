b8e363354108112d8089f72e4ce9ba13b9f4cd25
Aleksandr Kuznetsov <aleksandr_kuznetsov3@epam.com>
2020-03-30 18:31:15 +0400

    Add file extension filter and change path processing

--- mllib/reader.py
+++ mllib/reader.py
@@ -3,19 +3,20 @@ import numpy as np
 import os
 from copy import deepcopy
 from abc import ABC, abstractmethod
-from typing import Optional, List, Callable, Iterator, Any
+from typing import Optional, List, Callable, Iterator, Any, Union
 import matplotlib.image as mpimg
 
 
 class Reader(ABC):
 
-    def __init__(self, folder: str,
+    def __init__(self, paths: Union[str, List[str]],
                  file_filter: Optional[Callable] = None,
                  sorted_key: Optional[Callable] = None,
                  concatenate_method: Optional[Callable] = None,
                  batch_size: int = -1):
         assert batch_size > 0 or batch_size == -1, f"Batch size should be > 0 or == -1, received {batch_size}"
-        self.__files = sorted(self.__get_files_paths(folder, file_filter), key=sorted_key)
+        paths = [paths] if isinstance(paths, str) else paths
+        self.__files = sorted(self.__get_files_paths(paths, file_filter), key=sorted_key)
         self.__concatenate_method = lambda x: x if concatenate_method is None else concatenate_method
         self.__batch_size = batch_size
         self.__batch = []
@@ -34,15 +35,25 @@ class Reader(ABC):
         other.__files = other.__files[item]
         return other
 
-    @staticmethod
-    def __get_files_paths(folder: str, file_filter: Callable) -> List[str]:
+    def __get_files_paths(self, paths: List[str], file_filter: Callable) -> List[str]:
         paths_list = []
-        for root, folders_list, files_list in os.walk(os.path.abspath(folder)):
-            for file in files_list:
-                paths_list.append(os.path.join(root, file))
+        for path in paths:
+            path = os.path.abspath(path)
+            if os.path.isfile(path):
+                paths_list.append(path)
+            elif os.path.isdir(path):
+                paths_list += self.__parse_folder(path)
         if file_filter is not None:
             paths_list = list(filter(file_filter, paths_list))
         return paths_list
+
+    @staticmethod
+    def __parse_folder(folder: str):
+        files = []
+        for root, _, files_list in os.walk(folder):
+            for file in files_list:
+                files.append(os.path.join(root, file))
+        return files
 
     def __compile_batch(self, data: Any):
         if self.__batch_size == -1:
@@ -58,15 +69,19 @@ class Reader(ABC):
         raise NotImplemented
 
 
-class TimeSeriesCSVReader(Reader):
+class CSVReader(Reader):
 
-    def __init__(self, folder: str,
+    def __init__(self, paths: str,
                  file_filter: Optional[Callable] = None,
                  sorted_key: Optional[Callable] = None,
                  concatenate_method: Optional[Callable] = None,
                  batch_size: int = -1,
                  chunk_size: int = 10000):
-        super().__init__(folder, file_filter, sorted_key, concatenate_method, batch_size)
+        if callable(file_filter):
+            complex_filter = lambda x: file_filter(x) and x.split(".")[-1] == "csv"
+        else:
+            complex_filter = lambda x: x.split(".")[-1] == "csv"
+        super().__init__(paths, complex_filter, sorted_key, concatenate_method, batch_size)
         self._chunk_size = chunk_size
 
     def _create_iterator(self, files_paths: List[str]) -> Iterator[dict]:
@@ -83,12 +98,16 @@ class TimeSeriesCSVReader(Reader):
 
 class ImageReader(Reader):
 
-    def __init__(self, folder: str,
+    def __init__(self, paths: str,
                  file_filter: Optional[Callable] = None,
                  sorted_key: Optional[Callable] = None,
                  concatenate_method: Optional[Callable] = None,
                  batch_size: int = -1):
-        super().__init__(folder, file_filter, sorted_key, concatenate_method, batch_size)
+        if callable(file_filter):
+            complex_filter = lambda x: file_filter(x) and x.split(".")[-1] in {"jpg", "jpeg", "png", "tiff"}
+        else:
+            complex_filter = lambda x: x.split(".")[-1] in {"jpg", "jpeg", "png", "tiff"}
+        super().__init__(paths, complex_filter, sorted_key, concatenate_method, batch_size)
 
     def _create_iterator(self, files_paths: List[str]) -> Iterator[np.ndarray]:
         for path in files_paths:
@@ -96,9 +115,9 @@ class ImageReader(Reader):
 
 
 if __name__ == "__main__":
-    FOLDER = r"C:\Projects\Customers\ConocoFillips\data\raw\crosslines"
+    FOLDER = r"C:\Projects\Customers\ConocoFillips\data\interim"
     reader = ImageReader(
-        folder=FOLDER
+        paths=FOLDER
     )
 
     for r in reader:
